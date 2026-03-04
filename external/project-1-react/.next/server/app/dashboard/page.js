39c6a1426639f6c67f80e8c17b59530294ebf52a
떵히 <sunghee@tteonghiui-MacBookPro.local>
2026-03-05 00:24:08 +0900

    chore: remove external app build cache from repo

--- external/project-1-react/.next/server/app/dashboard/page.js
+++
@@ -1,6 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/dashboard/page.js";
-const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules__pnpm_5bb7b6._.js");
-runtime.loadChunk("server/chunks/ssr/_8e2231._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/dashboard/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@14.2.16_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/next/dist/esm/build/templates/app-page.js?page=/dashboard/page { COMPONENT_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_1 => \"[project]/app/loading.tsx [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_2 => \"[project]/node_modules/.pnpm/next@14.2.16_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\", COMPONENT_3 => \"[project]/app/dashboard/page.tsx [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript) <facade>", CHUNK_PUBLIC_PATH).exports;
--- external/project-1-react/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"g+bdlf6a5ZDX+NbtC3CywpW7jHz12dalXi+G3Uiwar4=\"\n}"
